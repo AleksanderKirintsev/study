6483e3b798a82b10083d68bee34395d8ea6d0eb1
Aleksander Kirintsev <kirintsev00@mail.ru>
2018-07-15 20:32:39 +0300

    1

---
+++ Task№32.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+srand(time(NULL));
+const int N = 10;
+int a[N];
+int min1,min2;
+
+for (int i = 0; i < N; ++i)
+a[i] = 1 + rand()%10; // случайные числа от 1 до 10
+
+min1 = a[0];
+min2 = a[1];
+for (int i = 0; i < N; ++i)
+cout << a[i] << " ";
+
+cout << endl;
+for (int i = 0; i < N; ++i)
+for (int j = 0; j < N; ++j)
+if (abs(a[i] + a[j]) < abs(min1 + min2) && i != j)
+{
+min1 = a[i];
+min2 = a[j];
+}
+cout << min1 << " " << min2;
+
+return 0;
+}
