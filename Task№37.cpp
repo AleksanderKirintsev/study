685fbfcf9695e3b7032d9399cf0d0656948bb7a2
Aleksander Kirintsev <kirintsev00@mail.ru>
2018-07-16 22:34:38 +0300

    task

--- Task№37.cpp
+++ Task№37.cpp
@@ -5,41 +5,41 @@
 using namespace std;
 
 int main() {
-srand(time(NULL));
-const int N = 30;
-int a[N];
-int l=1,lMax=0,sum = 0,max_sum = 0;
+    srand(time(NULL));
+    const int N = 30;
+    int a[N];
+    int l=1,lmax=0,s = 0,smax = 0;
 
-for (int i = 0; i < N; ++i)
-a[i] = 1 + rand()%10; // случайные числа от 1 до 10
+    for (int i = 0; i < N; ++i)
+        a[i] = 1 + rand()%10; // случайные числа от 1 до 10
 
-for (int i = 0; i < N; ++i)
-cout << a[i] << " ";
-cout << endl;
+    for (int i = 0; i < N; ++i)
+        cout << a[i] << " ";
+    cout << endl;
 
-for (int i = 0; i < N-1; ++i)
-if(a[i] < a[i+1])
-{
+    for (int i = 0; i < N-1; ++i)
+        if(a[i] < a[i+1])
+        {
 
-l++;
-sum += a[i];
+            l++;
+            s += a[i];
 
-}else
-{
-sum += a[i];
-if(l == lMax && sum > max_sum )
-{
-max_sum = sum;
-}else
-if (l > lMax)
-{
-lMax = l;
-max_sum = sum;
-}
+        }else
+        {
+            s += a[i];
+            if(l == lmax && s > smax )
+            {
+                smax = s;
+            }else
+                if (l > lmax)
+                {
+                    lmax = l;
+                    smax = s;
+                }
 
-sum = 0;
-l = 1;
-}
-cout << max_sum;
-return 0;
+            s = 0;
+            l = 1;
+        }
+    cout << smax;
+    return 0;
 }
