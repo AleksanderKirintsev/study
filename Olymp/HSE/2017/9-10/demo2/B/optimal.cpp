2f5915e9f68403d9325166c980fe387d90c0da4f
Aleksander Kirintsev <kirintsev00@mail.ru>
2018-09-22 10:58:03 +0300

    change demo2/B

--- Olymp/HSE/2017/9-10/demo2/B/optimal.cpp
+++ Olymp/HSE/2017/9-10/demo2/B/optimal.cpp
@@ -3,25 +3,18 @@
 using namespace std;
 
 int main() {
-    int a, b, c, d, n, ans[10];
-    cin >> n;
-    for ( int i = 0; i < n; i++ ) {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    cout.tie(0);
 
-        cin >> a >> b >> c >> d;
-        if (b == 1 && b * c + a == d) ans[i] = 1;
-        else if (b == 1 && b * c + a != d ) ans[i] = 0;
-        else {
-        if ( b * c + a <= d )
-            ans[i] = 1;
-        else
-            ans[i] = 0;
-            }
-    }
+    int a, b, c, d, n;
+    cin >> n;
+
     for ( int i = 0; i < n; i++ ) {
-        if ( ans[i] == 1 )
-            cout << "YES" << endl;
-        else
-            cout << "NO" << endl;
+        cin >> a >> b >> c >> d;
+        cout << ((b == 1 && b * c + a == d) || (b > 1 && b * c + a <= d) ? "YES" : "NO") << '\n';
+
     }
+
     return 0;
 }
