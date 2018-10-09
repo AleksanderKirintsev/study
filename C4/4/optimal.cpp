8d4e74cee117e01303da64589262ee9ccb28e08e
Aleksander Kirintsev <kirintsev00@mail.ru>
2018-10-09 21:46:48 +0300

    corrected code

--- C4/4/optimal.cpp
+++ C4/4/optimal.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-   int n,ans = 0;
-   double a, m = 1e6;
-   cin >> n;
-   for (int i = 0; i < n; i++){
-    cin >> a;
-    if (a > 1.0000000)
-        {
-        ans++;
-        m = min(a, m);
-   }
-   }
-   cout << ans <<  " " << m;
+int main() {
+    int n, ans = 0;
+    double a, m = 1e6;
+
+    cin >> n;
+
+    for ( int i = 0; i < n; i++ ) {
+        cin >> a;
+
+        if ( a > 1.0000000 ) {
+            ans++;
+            m = min( a, m );
+        }
+    }
+    cout << ans <<  " " << m;
     return 0;
 }
