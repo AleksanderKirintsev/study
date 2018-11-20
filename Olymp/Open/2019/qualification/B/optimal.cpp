16df43342e528ed399ade26e92cb05db687bf493
Aleksander Kirintsev <kirintsev00@mail.ru>
2018-11-20 19:00:29 +0300

    B/optimal

--- Olymp/Open/2019/qualification/B/optimal.cpp
+++ Olymp/Open/2019/qualification/B/optimal.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main() {
     int n;
-    string s;
-    cin >> n >> s;
+    string s;       // char 16 бит, n = 1 000 000
+    cin >> n >> s; //1 000 000
 
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 0; i < n - 1; i++) //10 000 000
         if (s[i] != s[i + 1]) {
             cout << "Yes\n" << i+1 << " " << i + 2 << " " << 1;
             return 0;
