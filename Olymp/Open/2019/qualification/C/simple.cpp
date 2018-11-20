a036c79ba290ccf9859063ebb4467e25bb483689
Aleksander Kirintsev <kirintsev00@mail.ru>
2018-11-20 20:19:19 +0300

    task Open/2019/qualification/C/simple

---
+++ Olymp/Open/2019/qualification/C/simple.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define F first
+#define S second
+int main(){
+    freopen("tests/01","r",stdin);
+    int n,p,k,m,sum = 0;
+    cin >> n >> p >> k;
+    pair<int,int> a[n];
+
+    for (int i = 0; i < n; i++){
+        cin >> m;
+        a[i].F = m / k;
+        a[i].S = m % k;
+    }
+    sort (a, a + n, [](const pair<int,int> &l, const pair<int,int> &r) {return l > r;});
+
+    for(int i = 0; i < n; i++){
+        if (p - (k - a[i].S) >= 0){
+            p -=(k - a[i].S);
+            a[i].S+=k-a[i].S;
+
+            a[i].F += a[i].S / k;
+            a[i].S = a[i].S % k;
+        }else break;
+    }
+
+    for (int i = 0; i < n; i++)
+        sum+=a[i].F;
+    cout << sum + p/k;
+    return 0;
+}
