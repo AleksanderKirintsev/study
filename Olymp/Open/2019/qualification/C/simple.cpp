297acb0dbc52733ccffc30f6dd7b6788452e6ef3
Aleksander Kirintsev <kirintsev00@mail.ru>
2018-11-29 18:32:45 +0300

    Olymp\Open\2019\qualification\C

--- Olymp/Open/2019/qualification/C/simple.cpp
+++ Olymp/Open/2019/qualification/C/simple.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-#define F first
-#define S second
+#define Q first
+#define R second
 int main(){
     long long int n,p,k,m,sum = 0;
     cin >> n >> p >> k;
@@ -10,23 +10,23 @@ int main(){
 
     for (int i = 0; i < n; i++){
         cin >> m;
-        a[i].F = m / k;
-        a[i].S = m % k;
+        a[i].Q = m / k;
+        a[i].R = m % k;
     }
-    sort (a, a + n, [](const pair<int,int> &l, const pair<int,int> &r) {return l > r;});
+    sort (a, a + n, [](const pair<int,int> &l, const pair<int,int> &r) {return l.R > r.R;});
 
     for(int i = 0; i < n; i++){
-        if (p - (k - a[i].S) >= 0){
-            p -=(k - a[i].S);
-            a[i].S+=k-a[i].S;
+        if (p - (k - a[i].R) >= 0){
+            p -=(k - a[i].R);
+            a[i].R+=k-a[i].R;
 
-            a[i].F += a[i].S / k;
-            a[i].S = a[i].S % k;
+            a[i].Q += a[i].R / k;
+            a[i].R = a[i].R % k;
         }else break;
     }
 
     for (int i = 0; i < n; i++)
-        sum+=a[i].F;
+        sum+=a[i].Q;
     cout << sum + p / k;
     return 0;
 }
