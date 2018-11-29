0778a19d03682b44b896f7b5d06088054caac71d
Aleksander Kirintsev <kirintsev00@mail.ru>
2018-11-29 18:34:57 +0300

    Olymp\HSE\2018\9-10\demo1\C

--- Olymp/HSE/2018/9-10/demo1/C/optimal.cpp
+++ Olymp/HSE/2018/9-10/demo1/C/optimal.cpp
@@ -2,22 +2,21 @@
 using namespace std;
 
 int main() {
-    int n, m, s = 0, w = 1, q;
+    int n, m, s = 0, w = 1, q = 0;
     char c;
     cin >> n >> m;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++) {
             cin >> c;
-            if (c == '.') {
+            if (c == '.')
                 s += !w;
-                else {
-                    w = !w;
-                    q++;
-                }
+            else {
+                w = !w;
+                q++;
             }
-
-
-            cout << s + q / 2;
-            return 0;
         }
+
+    cout << s + q / 2;
+    return 0;
+}
