566655d541f2c495a328ad68a32c23164a3113ac
Aleksanderkirintsev <dg2z8u5p>
2018-12-02 13:49:31 +0300

    task D

--- Olymp/Open/2019/qualification/D/optimal1.cpp
+++ Olymp/Open/2019/qualification/D/optimal1.cpp
@@ -1,35 +1,11 @@
 #include <iostream>
+#include <fstream>
+#include <ctime>
 using namespace std;
 
 int main() {
-    cin.tie(0);
-    cin.sync_with_stdio(0);
-    cout.tie(0);
-
-    int n,q,l,r;
-    cin >> n >> q;
-
-    int a[n];
-    for(int i = 0; i < n; i++)
-        cin >> a[i];
-
-    for(int o = 0; o < q; o++) {
-        char c;
-        cin >> c >> l >> r;
-        l--;
-        r--;
-        if(c == '!')
-            a[l] = r+1;
-        else {
-            int b[n+1] = {0};
-            for(int i = l; i <= r; i++)
-                b[a[i]]++;
-            for(int i = 0; i <= n; i++)
-                if(b[i] == 0) {
-                    cout << i << '\n';
-                    break;
-                }
-        }
-    }
+    srand(time(0));
+    for(int i = 0; i < 80; i++)
+        cout << rand() % 51 << " ";
     return 0;
 }
