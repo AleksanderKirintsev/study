8b65a3c8c428843faa9b8d2df866ac289973cd53
AleksanderKirintsev <kirintsev00@mail.ru>
2018-12-10 22:23:51 +0300

    Olymp\Open\2019\qualification\D\optimal2

--- Olymp/Open/2019/qualification/D/optimal2.cpp
+++ Olymp/Open/2019/qualification/D/optimal2.cpp
@@ -2,7 +2,7 @@
 #include <bitset>
 using namespace std;
 
-unsigned int n,q,a[500001];
+int n,q,a[500001];
 
 int main() {
     cin.tie(0);
@@ -16,24 +16,19 @@ int main() {
     for(int i = 0; i < q; i++) {
         char c;
         int l,r;
-
         cin >> c >> l >> r;
 
         if(c == '?') {
-            unsigned long long int b[2] = {0};
-            for(int j = l; j <= r; j++) {
-                b[a[j]/64] |= ((unsigned long long)(1 << a[j] % 64));
-                printf("%2d %2d ",j,a[j]);
-                cout << bitset<sizeof (long long)* 8>(*(long long int*)(&b[0])) << " " << b[0] <<endl;
-            }
-
+            unsigned long long int b[7813] = {0};
+            for(int j = l; j <= r; j++)
+                b[a[j]/64] |= ((unsigned long long)1 << a[j] % 64);
 
             int j;
-            for(j = 0; b[j] == (1<<64);)
-                j++;
+            unsigned long long int s = ((unsigned long long)1 << 63)+ (((unsigned long long)1 << 63)-1);
+            for(j = 0; b[j] == s; j++) {}
 
-            for (int d = 0; d < 64; d++)
-                if ((b[j] & (1 << d)) == 0) {
+            for (unsigned long long int d = 0; d < 64; d++)
+                if ((b[j] & ((unsigned long long)1 << d)) == 0) {
                     cout << j*64+d << '\n';
                     break;
                 }
