25c799bf39975d663b21832432ab77edc1c6ccf6
AleksanderKirintsev <kirintsev00@mail.ru>
2019-02-28 16:56:11 +0300

    D optimal

--- Olymp/Moscow/2019/qualification2/D/optimal.cpp
+++ Olymp/Moscow/2019/qualification2/D/optimal.cpp
@@ -3,26 +3,23 @@
 #include <set>
 #include <vector>
 using namespace std;
-int n,k,a[1000010];
+int n,k,*a;
 int main(){
-	freopen("tests/10","r",stdin);
-//    cin.tie(0);
-//    cin.sync_with_stdio(0);
-//    cout.tie(0);
+//	freopen("tests/10","r",stdin);
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    cout.tie(0);
     cin >> n >> k;
+    a = new int[n];
 
     for (int i = 0; i < n; i++)
         cin >> a[i];
     multiset<int> ms(a,a+k);
-//    for(auto x : ms)
-//        cout << x << " ";
-
-    for (int i = k; i < n; i++){
         auto iter = ms.begin();
-        auto key = ms.find(a[i-k]);
         advance(iter,k/2);
-//        for(auto x : ms)
-//            cout << x << " ";
+
+    for (int i = k - 1; i < n; i++){
+        auto key = ms.find(a[i-k+1]);
         cout << *iter << " ";
         ms.erase(key);
         ms.insert(a[i+1]);
