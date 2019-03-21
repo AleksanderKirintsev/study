b0f863933551db6c4f1dcd9349dbd1a048e14fa2
AleksanderKirintsev <kirintsev00@mail.ru>
2019-03-21 16:40:43 +0300

    optimal 1

--- Custom/Recursion/2/optimal1.cpp
+++ Custom/Recursion/2/optimal1.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
-
 using namespace std;
 
 int f(int n,int k) {
     if (n == 0)
         return 1;
-
+    else if(k == 0)
+        return 0;
     int sum = 0;
-    for(int i = 1; i < k; ++i)
-        if(n - i >=0)
-            sum += f(n-i,i);
+    for(int i = min(n,k); n-i <= ((i-1)*(i-1)+(i-1))/2; --i)
+        sum += f(n-i,i-1);
 
     return sum;
 }
 int main() {
     int n;
     cin >> n;
-    cout << f(n,n+1);
+    cout << f(n,n);
     return 0;
 }
 
