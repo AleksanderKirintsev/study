a7cd8688c580d15974329af0a55bae244862099f
AleksanderKirintsev <kirintsev00@mail.ru>
2019-05-02 19:01:37 +0300

    simple

--- Custom/Recursion/5/simple.cpp
+++ Custom/Recursion/5/simple.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 
 int m,n,k,*s;
+int moves[4][2]; {{-1,0},{0,1},{1,0},{0,-1}};
 
 int main(){
-    freopen("tests/10","r",stdin);
+    //freopen("tests/10","r",stdin);
     cin >> m >> n >> k;
     s = new int[m*n]{0};
     for(int i = 0; i < k; i++){
