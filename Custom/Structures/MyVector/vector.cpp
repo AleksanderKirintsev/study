dd00eead739b1d507dc95e716e9f0c70542394a3
AleksanderKirintsev <kirintsev00@mail.ru>
2019-06-03 21:37:16 +0300

    vector

--- Custom/Structures/MyVector/vector.cpp
+++ Custom/Structures/MyVector/vector.cpp
@@ -31,30 +31,52 @@ struct MyVector {
         }
         a[length++] = value;
     }
-    int size(){
-    return length;
+
+    int size() {
+        return length;
     }
 
-    int operator[](int index) {
+    int& operator[](int index) {
         return a[index];
     }
 
-    MyVector& operator= (const MyVector& value) {
-        for(int i = 0; i < this->capacity; i++)
-            a[i] = value.a[i];
+    MyVector& operator=(MyVector& vec) {
+        this->length = vec.size();
+        while(length > capacity) {
+            capacity *= 2;
+            int *b = new int[capacity];
+            memcpy(b,a,length * sizeof(int));
+            delete a;
+            a = b;
+        }
+
+        for(int i = 0; i < length; i++)
+            a[i] = vec[i];
+
         return *this;
     }
+
+
 };
 int main() {
-    MyVector vec;
-    vector<int> vect(1);
+    MyVector vec,vec1(4);
+
     vec.push_back(1);
     vec.push_back(2);
     vec.push_back(3);
-    //vec[0] = 5;
+
+    vec[0] = 1;vec[1] = 2;vec[2] = 3;
+    vec1[0] = 4;vec1[1] = 5;vec1[2] = 6;vec1[3]=7;
+
+    vec = vec1;
+    vec[0] = 3;
+
     for(int i = 0; i < vec.size(); i++)
         cout << vec[i];
-//    for(vector<int>::iterator it = vec.begin(); it != vec.end(); it++)
+    cout << endl;
+    for(int i = 0; i < vec1.size(); i++)
+        cout << vec1[i];
+//    for(MyVector::iterator it = vec.begin(); it != vec.end(); it++)
 //        cout << *it;
 
     return 0;
