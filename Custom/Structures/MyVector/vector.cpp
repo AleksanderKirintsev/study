88abe8756810a82c92de3fb6577eb421818e7026
AleksanderKirintsev <kirintsev00@mail.ru>
2019-06-12 17:15:09 +0300

    template added

--- Custom/Structures/MyVector/vector.cpp
+++ Custom/Structures/MyVector/vector.cpp
@@ -1,78 +1,63 @@
 #include <iostream>
 #include <cstring>
-#include <vector>
 using namespace std;
 
-struct MyVector {
-    int *a;
+template<typename T> struct vector {
+    T *a;
     int capacity;
     int length;
 
-
-    MyVector() {
+    vector() {
         capacity = 2;
-        a = new int[capacity];
+        a = new T[capacity];
         length = 0;
     }
 
-    MyVector(int capacity) {
-        this->capacity = capacity;
-        a = new int[this->capacity] {};
-        length = this->capacity;
+    vector(int capacity) {
+        this->capacity = length = capacity;
+        a = new T[capacity] {};
     }
 
-    void push_back(int value) {
+    void push_back(T value) {
         if(length == capacity) {
             capacity *= 2;
-            int *b = new int[capacity];
-            memcpy(b,a,length * sizeof(int));
+            T *b = new T[capacity];
+            memcpy(b,a,length * sizeof(T));
             delete a;
             a = b;
         }
         a[length++] = value;
     }
 
-    int size() {
-        return length;
-    }
+    int size() {return length;}
 
-    int& operator[](int index) {
-        return a[index];
-    }
+    T& operator[](int index) {return a[index];}
 
 
     struct Iterator{
-        int *it;
-        Iterator(int* first){
-            it = first;
-        }
+        T *it;
+        Iterator(T* first){it = first;}
 
-        int operator+ (int n) {return *(it+n);}
-        int operator- (int n) {return *(it-n);}
-
-        int operator++ (int) {return *it++;}
-        int operator-- (int) {return *it--;}
-        int operator++ () {return *++it;}
-        int operator-- () {return *--it;}
-
-        bool operator!= (const Iterator& iter) { return it != iter.it;}
-        bool operator== (const Iterator& iter) { return it == iter.it;}
-        int operator* () {return *it;}
+        Iterator operator++ () {return ++it;}
+        T operator* () {return *it;}
+        bool operator!= (const Iterator& iter) {return it != iter.it;}
     };
     Iterator begin(){return a;}
     Iterator end(){return a+length;}
 
 };
-int main() {
-    MyVector vec;
-    vec.push_back(1);
-    vec.push_back(2);
-    vec.push_back(3);
-    vec[0] = 5;
 
-    for(MyVector::Iterator it = vec.begin(); it != vec.end(); it++)
-            cout << *it;
+int main() {
+    vector<char> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3.5);
+    v[0] = 5.6;
+
+    for(auto x:v)
+        cout << x << " ";
 
     return 0;
 }
+
 
