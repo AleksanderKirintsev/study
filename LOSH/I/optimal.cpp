b8796ee3c58f245e85fb1f41ea77b66aa44bffe0
AleksanderKirintsev <kirintsev00@mail.ru>
2019-06-30 12:19:27 +0300

    optimal refactoring

--- LOSH/I/optimal.cpp
+++ LOSH/I/optimal.cpp
@@ -4,30 +4,31 @@
 #include <algorithm>
 using namespace std;
 
-#define P make_pair
-#define X first
-#define Y second
+#define Y first
+#define X second
 
 int n,h,v;
-vector<vector<int>> a;
+int a[1010][1010];
 deque<pair<int,int>> q;
-pair<int,int> moves[2][6]= {{P(-1,0),P(-1,1),P(0,1),P(1,1),P(1,0),P(0,-1)},{P(-1,-1),P(-1,0),P(0,1),P(1,0),P(1,-1),P(0,-1)}};
+int dx[6] ={0 ,  1,  1, 1,  0, -1};
+int dy[6] ={-1, -1,  0, 1,  1,  0};
 
-int V(){
+int void_processing(int y, int x){
     int sum = 0;
-    pair<int,int> p;
+    q.push_back({y,x});
+
     while(!q.empty()){
-        p = q.front();
+        pair<int,int> p = q.front();
         q.pop_front();
-        a[p.X][p.Y] = 1;
+        a[p.Y][p.X] = 1;
         sum++;
 
         for(int i = 0; i < 6; i++){
-            int x = p.X+moves[p.X%2][i].X;
-            int y = p.Y+moves[p.X%2][i].Y;
-            if(a[x][y] == 0){
-                a[x][y] = 1;
-                q.push_back({x,y});
+            y = p.Y+dy[i];
+            x = p.X+dx[i]-(p.Y%2 && dy[i]);
+            if(a[y][x] == 0){
+                a[y][x] = 1;
+                q.push_back({y,x});
             }
         }
     }
@@ -37,27 +38,30 @@ int V(){
 int main(){
     //freopen("tests/14","r",stdin);
     cin >> n >> h >> v;
-    a.resize(h+2,vector<int>(v+2,1));
 
-    for(int x = 1; x <= h; x++)
-        for(int y = 1; y <= v; y++){
+    fill_n(a[0],v+2,1);
+    fill_n(a[h+1],v+2,1);
+    for(int y = 1; y <= h; y++){
+        fill_n(a[y],v+2,1);
+        for(int x = 1; x <= v; x++){
             char c;
             cin >> c;
             if(c =='.')
-                a[x][y] = 0;
+                a[y][x] = 0;
         }
+    }
 
-    vector<int> volumes;
-    for(int x = 1; x <= h; x++)
-        for(int y = 1; y <= v; y++)
-            if(a[x][y] == 0){
-                q.push_back({x,y});
-                volumes.push_back(V());
-            }
+    int idx = 0;
+    int *volumes = new int[h*v]{};
+    for(int y = 1; y <= h; y++)
+        for(int x = 1; x <= v; x++)
+            if(a[y][x] == 0)
+                volumes[idx++] = void_processing(y,x);
+
+    sort(volumes,volumes+idx,greater<int>());
 
     int ans = 0;
-    sort(volumes.rbegin(),volumes.rend());
-    for(int i = 0; i < int(volumes.size()) && n > 0; i++,ans++)
+    for(int i = 0; i < idx && n > 0; i++,ans++)
         n -= volumes[i];
 
     cout << ans;
