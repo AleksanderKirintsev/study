5e745180f13f1400fde6c81d785f65e0d29e1c29
AleksanderKirintsev <kirintsev00@mail.ru>
2019-06-30 12:20:18 +0300

    optimal tests/10 - 6min

--- Olymp/Moscow/2019/qualification2/D/optimal.cpp
+++ Olymp/Moscow/2019/qualification2/D/optimal.cpp
@@ -1,28 +1,57 @@
 #include <iostream>
 #include <algorithm>
-#include <set>
-#include <vector>
 using namespace std;
-int n,k,*a;
-int main(){
-//	freopen("tests/10","r",stdin);
+int n,k,*a,*b;
+
+int del(int c){
+    int l = 0,r = k;
+    while(l+1 < r){
+        int mid = (l+r)/2;
+        if(b[mid] > c)
+            r = mid;
+        else
+            l = mid;
+    }
+
+    b[l] = 0;
+    return l;
+}
+void add(int c,int idx){
+    b[idx] = c;
+    while(b[idx] < b[idx-1] && idx > 0){
+        swap(b[idx],b[idx-1]);
+        idx--;
+    }
+    while(b[idx] > b[idx+1] && idx < k-1){
+        swap(b[idx],b[idx+1]);
+        idx++;
+    }
+}
+int main() {
+    //freopen("tests/00","r",stdin);
     cin.tie(0);
     cin.sync_with_stdio(0);
     cout.tie(0);
+
     cin >> n >> k;
+
     a = new int[n];
+    b = new int[k];
 
-    for (int i = 0; i < n; i++)
+
+    for(int i = 0; i < n; i++)
         cin >> a[i];
-    multiset<int> ms(a,a+k);
-        auto iter = ms.begin();
-        advance(iter,k/2);
 
-    for (int i = k - 1; i < n; i++){
-        auto key = ms.find(a[i-k+1]);
-        cout << *iter << " ";
-        ms.erase(key);
-        ms.insert(a[i+1]);
+    for(int i = 0; i < k; i++)
+        b[i] = a[i];
+
+    sort(b,b+k);
+
+    for(int i = k; i < n; i++){
+        cout << b[k/2] << " ";
+        int idx = del(a[i-k]);
+        add(a[i],idx);
     }
+    cout << b[k/2] << " ";
     return 0;
 }
