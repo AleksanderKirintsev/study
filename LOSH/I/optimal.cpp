3fe7558639d7cd666e00dc0e019311ae420975bb
AleksanderKirintsev <kirintsev00@mail.ru>
2019-07-04 17:47:27 +0300

    optimal

--- LOSH/I/optimal.cpp
+++ LOSH/I/optimal.cpp
@@ -4,31 +4,31 @@
 #include <algorithm>
 using namespace std;
 
-#define Y first
-#define X second
+#define X first
+#define Y second
 
 int n,h,v;
-int a[1010][1010];
+int **a;
 deque<pair<int,int>> q;
-int dx[6] ={0 ,  1,  1, 1,  0, -1};
-int dy[6] ={-1, -1,  0, 1,  1,  0};
+int dx[6] ={-1, -1,  0, 1,  1,  0};
+int dy[6] ={0 ,  1,  1, 1,  0, -1};
 
-int void_processing(int y, int x){
+int void_processing(int x, int y){
     int sum = 0;
-    q.push_back({y,x});
+    q.push_back({x,y});
 
     while(!q.empty()){
         pair<int,int> p = q.front();
         q.pop_front();
-        a[p.Y][p.X] = 1;
+        a[p.X][p.Y] = 1;
         sum++;
 
         for(int i = 0; i < 6; i++){
-            y = p.Y+dy[i];
-            x = p.X+dx[i]-(p.Y%2 && dy[i]);
-            if(a[y][x] == 0){
-                a[y][x] = 1;
-                q.push_back({y,x});
+            x = p.X+dx[i];
+            y = p.Y+dy[i]-(p.X%2 && dx[i]);
+            if(a[x][y] == 0){
+                a[x][y] = 1;
+                q.push_back({x,y});
             }
         }
     }
@@ -36,27 +36,34 @@ int void_processing(int y, int x){
 }
 
 int main(){
-    //freopen("tests/14","r",stdin);
+    //freopen("tests/00","r",stdin);
     cin >> n >> h >> v;
+    a = new int*[h+2];
+    for(int i = 0; i < h+2; i++){
+        a[i] = new int[v+2];
+        fill_n(a[i],v+2,1);
+    }
 
-    fill_n(a[0],v+2,1);
-    fill_n(a[h+1],v+2,1);
-    for(int y = 1; y <= h; y++){
-        fill_n(a[y],v+2,1);
-        for(int x = 1; x <= v; x++){
+    for(int i = 0; i < h+2; i++){
+        for(int j = 0; j < h+2; j++)
+            cout << a[i][j] << " ";
+        cout << endl;
+    }
+    for(int x = 1; x <= h; x++){
+        for(int y = 1; y <= v; y++){
             char c;
             cin >> c;
             if(c =='.')
-                a[y][x] = 0;
+                a[x][y] = 0;
         }
     }
 
     int idx = 0;
     int *volumes = new int[h*v]{};
-    for(int y = 1; y <= h; y++)
-        for(int x = 1; x <= v; x++)
-            if(a[y][x] == 0)
-                volumes[idx++] = void_processing(y,x);
+    for(int x = 1; x <= h; x++)
+        for(int y = 1; y <= v; y++)
+            if(a[x][y] == 0)
+                volumes[idx++] = void_processing(x,y);
 
     sort(volumes,volumes+idx,greater<int>());
 
