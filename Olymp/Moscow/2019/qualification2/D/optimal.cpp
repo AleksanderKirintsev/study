9db7aacb16cf8f09372354c2f09d6edbe9699eba
AleksanderKirintsev <kirintsev00@mail.ru>
2019-07-22 21:00:35 +0300

    optimal & human_algorithm

--- Olymp/Moscow/2019/qualification2/D/optimal.cpp
+++ Olymp/Moscow/2019/qualification2/D/optimal.cpp
@@ -2,10 +2,10 @@
 #include <algorithm>
 #include <cstring>
 using namespace std;
-int n,k,*a,*b;
+int n,k,*a,*b,*p;
 
 int main() {
-    //freopen("tests/01","r",stdin);
+    //freopen("tests/09","r",stdin);
     cin.tie(0);
     cin.sync_with_stdio(0);
     cout.tie(0);
@@ -14,29 +14,31 @@ int main() {
 
     a = new int[n];
     b = new int[k];
-
     for(int i = 0; i < n; i++)
         cin >> a[i];
 
     memcpy(b,a,sizeof(int)*k);
     sort(b,b+k);
+    memcpy(p,b,sizeof(int)*k);
+
+    cout << b[k/2] << " ";
 
     for(int i = k; i < n; i++) {
-        cout << b[k/2] << " ";
-
-        int del_idx = lower_bound(b,b+k,a[i-k])-b, ins_idx = lower_bound(b,b+k,a[i])-b-(a[i] > b[k-1]);
-        if(del_idx > ins_idx) {
-            memcpy(b+ins_idx+1,b+ins_idx,sizeof(int)*(del_idx-ins_idx));
-            ins_idx+=(b[ins_idx] < a[i]);
-        }else if(del_idx < ins_idx) {
-            memcpy(b+del_idx,b+del_idx+1,sizeof(int)*(ins_idx-del_idx));
-            ins_idx-=(b[ins_idx] > a[i]);
+        int del = a[i-k],ins = a[i];
+        if(del > ins) {
+            int del_idx = lower_bound(b,b+k,del) - b;
+            int ins_idx = upper_bound(b,b+k,ins) - b;
+            memmove(b+ins_idx+1,b+ins_idx,sizeof(int)*(del_idx-ins_idx));
+            b[ins_idx] = ins;
+        }else if(del < ins) {
+            int del_idx = upper_bound(b,b+k,del) - b - 1;
+            int ins_idx = lower_bound(b,b+k,ins) - b - 1;
+            memmove(b+del_idx,b+del_idx+1,sizeof(int)*(ins_idx-del_idx));
+            b[ins_idx] = ins;
         }
-
-        b[ins_idx] = a[i];
+        cout << b[k/2] << " ";
 
     }
 
-    cout << b[k/2] << " ";
     return 0;
 }
