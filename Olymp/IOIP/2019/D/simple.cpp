886081a2a61bc291917527021ac74864b8f22d52
AleksanderKirintsev <kirintsev00@mail.ru>
2019-08-26 19:11:30 +0300

    simple

--- Olymp/IOIP/2019/D/simple.cpp
+++ Olymp/IOIP/2019/D/simple.cpp
@@ -1,39 +1,30 @@
 #include <iostream>
-#include <cstdio>
-#include <set>
 using namespace std;
 #define ll long long
 #define a(x,y) a[x*(m+2)+y]
 #define b(x,y) b[x*(m+2)+y]
-ll n,m,p,*a,*b,ans;
-
+ll n,m,p,*a;
 
 int main() {
-    //freopen("tests/10","r",stdin);
     cin >> n >> m >> p;
     a = new ll[(n+2)*(m+2)]{};
-    b = new ll[(n+2)*(m+2)]{};
-
-    for(ll i = 1; i <= n; i++){
-        for(ll j = 1; j <= m; j++)
-            cin >> a(i,j);
-        b(i,1) = a(i,1);
-    }
 
     for(ll i = 1; i <= n; i++)
-        for(ll j = 2; j <= m; j++)
-            b(i,j) += b(i,(j-1))+a(i,j);
-    for(ll i = 2; i <= n; i++)
         for(ll j = 1; j <= m; j++)
-            b(i,j) += b((i-1),j);
+            cin >> a(i,j);
 
-    for(int x1 = 1; x1 <= n; x1++)
-        for(int y1 = 1; y1 <= m; y1++)
-            for(int x = x1; x <= n; x++)
-                for(int y = y1; y <= m; y++){
-                    ll sum = b(x,y)-b(x,(y1-1))-b((x1-1),y);
-                    if(sum % p == 0)
-                        ans = max(ans,sum);
+    ll *b = new ll[(n+2)*(m+2)]{};
+    for(ll i = 1; i <= n; i++)
+        for(ll j = 1,t = 0; j <= m; j++)
+            b(i,j) = (t+=a(i,j))+b((i-1),j);
+
+    ll ans = 0;
+    for(int x0 = 1; x0 <= n; x0++)
+        for(int x1 = x0; x1 <= n; x1++)
+            for(int y0 = 1; y0 <= m; y0++)
+                for(int y1 = y0; y1 <= m; y1++){
+                    ll sum = b(x1,y1)-b(x1,(y0-1))-b((x0-1),y1)+b((x0-1),(y0-1));
+                    ans = max(ans,(sum % p ? 0 : sum));
                 }
     cout << ans;
     return 0;
