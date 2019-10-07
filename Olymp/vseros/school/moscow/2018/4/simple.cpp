ba2a202716627115212580e9e74c95b254235397
AleksanderKirintsev <kirintsev00@mail.ru>
2019-10-07 20:27:04 +0300

    vseros\school\moscow\2018\4\

---
+++ Olymp/vseros/school/moscow/2018/4/simple.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+int n, *a;
+int main() {
+// блок чтения данных
+    cin >> n;
+    a = new int[n];
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+
+//блок заполнения вспомогательной структуры
+    bool coupes[9];//1 - купе свободно, 0 - купе занято
+    //тут будет код заполнения структуры, т.е. по оканчении его i-e купе полностью свободно, если coupes[i]==1
+    int u[6] = {1,2,3,4,53,54},v[55]={};
+    for(int i = 0; i < n; i++)
+        v[a[i]] = 1;
+    for(int i = 0; i < 9; i++){
+        coupes[i] = 1;
+        for(int j = 0; j < 6; j++)
+            coupes[i]*=v[u[j]];
+        for(int j = 0; j < 4; j++)
+            u[j]+=4;
+        u[4]-=2;
+        u[5]-=2;
+    }
+
+//блок обработки вспомогательной структуры
+    int mx = 0;
+    for(int i = 0, tmp = 0; i < 9; i++)
+            mx = max(tmp = (tmp+coupes[i])*coupes[i],mx);
+    cout << mx;
+
+
+
+
+
+
+
+
+
+
+
+    while(u[0] != 33){
+        if(v[u[0]] && v[u[1]] && v[u[2]] && v[u[3]] && v[u[4]] && v[u[5]])
+            tmp++;
+        else
+            tmp = 0;
+        mx = max(mx,tmp);
+        u[0]+=4;
+        u[1]+=4;
+        u[2]+=4;
+        u[3]+=4;
+        u[4]-=2;
+        u[5]-=2;
+    }
+    cout << mx;
+    return 0;
+}
