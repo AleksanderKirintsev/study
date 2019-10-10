a918af939f1ef5ec0fdc2f07f3c4e8a0b2308eb3
AleksanderKirintsev <kirintsev00@mail.ru>
2019-10-10 19:06:49 +0300

    Olymp\vseros\moscow\2018\3\simple

--- Olymp/vseros/moscow/2018/3/simple.cpp
+++ Olymp/vseros/moscow/2018/3/simple.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
-int h,m,s;
-int main() {
-    int ts[3] = {};
-    for(int i = 0; i < 3 ; i++){
-        scanf("%d:%d:%d",&h,&m,&s);
-        ts[i] = h*3600+m*60+s;
+const int DAY = 24*3600;
+struct Time{
+    int h,m,s;
+};
+
+istream& operator>> ( istream& in, Time& t) {
+        char c;
+        return in >> t.h >> c >> t.m >> c >> t.s;
     }
 
-    ts[2]+=(ts[0] > ts[2] ? 86400 : 0);
-    ts[1] += (ts[2] - ts[0])/2;
-    ts[1]%=86400;
-
-    printf("%02d:%02d:%02d",ts[1]/3600,ts[1]%3600/60,ts[1]%3600%60);
+Time a,b,c;
+int ttos(Time& t){
+    return t.h*3600+t.m*60+t.s;
+}
+Time stot(int s){
+    return {s/3600,s%3600/60,s%60};
+}
+int main() {
+    cin >> a >> b >> c;
+    int s1,s2,s3;
+    s1 = ttos(a),s2 = ttos(b),s3 = ttos(c);
+    s3+=(s1>s3 ? DAY : 0);
+    s2 = (s2 + (s3-s1)/2)% DAY;
+    printf("%02d:%02d:%02d",s2/3600,s2%3600/60,s2%60);
     return 0;
 }
