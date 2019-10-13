5878a041aa720e73bebfc26f0dd1409b1a4e2794
AleksanderKirintsev <kirintsev00@mail.ru>
2019-10-13 16:29:06 +0300

    Olymp\vseros\school\moscow\2018\4\simple

--- Olymp/vseros/school/moscow/2018/4/simple.cpp
+++ Olymp/vseros/school/moscow/2018/4/simple.cpp
@@ -29,30 +29,5 @@ int main() {
     for(int i = 0, tmp = 0; i < 9; i++)
             mx = max(tmp = (tmp+coupes[i])*coupes[i],mx);
     cout << mx;
-
-
-
-
-
-
-
-
-
-
-
-    while(u[0] != 33){
-        if(v[u[0]] && v[u[1]] && v[u[2]] && v[u[3]] && v[u[4]] && v[u[5]])
-            tmp++;
-        else
-            tmp = 0;
-        mx = max(mx,tmp);
-        u[0]+=4;
-        u[1]+=4;
-        u[2]+=4;
-        u[3]+=4;
-        u[4]-=2;
-        u[5]-=2;
-    }
-    cout << mx;
     return 0;
 }
