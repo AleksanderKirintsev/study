666157059e1e3f07f8d19cb9c6a9025350d20558
AleksanderKirintsev <kirintsev00@mail.ru>
2019-10-28 17:52:08 +0300

    Olymp\vsesib\selection\2020\B\simple

--- Olymp/vsesib/selection/2020/B/simple.cpp
+++ Olymp/vsesib/selection/2020/B/simple.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
-#include <vector>
-#include <set>
 using namespace std;
 #define X first
 #define Y second
+typedef pair<int,int> point;
+typedef pair<int,int> vec;
+
 int n;
-vector<set<int>> dst;
-pair<int,int> p[2500];
+point *p;
+bool f(int a,int b,int c){
+    vec ab,ac;
+    ab ={p[b].X-p[a].X,p[b].Y-p[a].Y};
+    ac ={p[c].X-p[a].X,p[c].Y-p[a].Y};
+    return (ab.X*ac.X+ab.Y*ac.Y == 0);
+}
 int main() {
     cin >> n;
-    dst.resize(n);
-
+    p = new point[n];
     for(int i = 0; i < n; i++)
         cin >> p[i].X >> p[i].Y;
 
-    for(int i = 0; i < n; i++)
-        for(int j = i+1; j < n; j++)
-                dst[i].insert((p[i].X-p[j].X)*(p[i].X-p[j].X)+(p[i].Y-p[j].Y)*(p[i].Y-p[j].Y));
-
     int q = 0;
-    for(int i = 0; i < n; i++)
-        for(int j = i+1; j < n; j++){
-            int a = (p[i].X-p[j].X)*(p[i].X-p[j].X)+(p[i].Y-p[j].Y)*(p[i].Y-p[j].Y);
-        }
+    for(int a = 0; a < n - 2; a++)
+        for(int b = a+1; b < n - 1; b++)
+            for(int c = b+1; c < n; c++)
+                q+= f(a,b,c)+f(b,a,c)+f(c,a,b);
     cout << q;
     return 0;
 }
