370d5b568ec4d66f36174934694608a0e6a67f1f
AleksanderKirintsev <kirintsev00@mail.ru>
2019-11-21 18:52:50 +0300

    Olymp\HSE\2020\11\lvl1\B\optimal(not working)

--- Olymp/HSE/2020/11/lvl1/B/optimal.cpp
+++ Olymp/HSE/2020/11/lvl1/B/optimal.cpp
@@ -2,97 +2,85 @@
 #include <vector>
 #include <queue>
 using namespace std;
-int n,m,*mark,*d;
+int n,m,*r,*s,*t;
 vector<vector<int>> g;
-
-int bfs_1(int v){
+pair<int,int> F(int a, int *r) {
     queue<int> q;
-    q.push(v);
-    int d[n] = {};
-    while(!q.empty()){
-        v = q.front();
-        d[v] = 1;
+    q.push(a);
+    r[a] = 1;
+    while(!q.empty()) {
+        int v = q.front();
         q.pop();
-        for(int x: g[v]){
-            if(mark[x] < mark[v]){
-                if(d[x] == 1)
-                    return x;
+        for(int x: g[v]) {
+            if(!r[x]) {
+                r[x] = r[v]+1;
                 q.push(x);
             }
+            if(r[x] == r[a])
+                return{x,a};
         }
     }
-    return -1;
+    return {-1,-1};
 }
-vector<int> bfs(int v){
+vector<int> G(int s, int f) {
     queue<int> q;
-    q.push(v);
+    int *p = new int[n] {};
     vector<int> way;
-    mark[v] = 1;
-    while(!q.empty()){
-        v = q.front();
+    p[s] = 1;
+    q.push(s);
+    while(!q.empty()) {
+        int v = q.front();
         q.pop();
-        for(int x: g[v]){
-            if(!mark[x]){
+        for(int x:g[v])
+            if(!p[x]) {
+                p[x] = p[v]+1;
                 q.push(x);
-                mark[x] = mark[v]+1;
             }
-            if(mark[x] == mark[v]){
-                d = new int[n];
-                int st;
-                bfs_1(x);
-                st = bfs_1(v);
-                queue<int> dq;
-                v = st;
-                dq.push(v);
-                while(!dq.empty()){
-                    way.push_back(v);
-                    dq.pop();
-                    for(int x: g[v]){
-                        if(mark[x] < mark[v] && d[x] == 1){
-                            dq.push(x);
-                            v = x;
-                            way.push_back(v);
-                        }
-                    }
-                }
 
-                for(int x: g[v])
-                    st = (mark[x] == mark[v] ? x : st);
-                v = st;
-                while(!dq.empty()){
-                    way.push_back(v);
-                    dq.pop();
-                    for(int x: g[v]){
-                        if(mark[x] > mark[v] && d[x]){
-                            v = x;
-                            dq.push(x);
-                            way.push_back(v);
-                        }
-                    }
-                }
-                delete d;
-                break;
-            }
+    }
+    int curr = f;
+    way.push_back(curr);
+    for(int x : g[curr]) {
+        if(curr == x)
+            return way;
+        if(p[x] < p[curr]) {
+            way.push_back(curr);
+            curr = x;
         }
     }
-    return way;
+
+}
+return way;
 }
 int main() {
     cin >> n >> m;
     g.resize(n);
-    vector<int> ans
-    for(int i = 0; i < m; i++){
+    vector<int> ans;
+    r = new int[n] {};
+    s = new int[n] {};
+    t = new int[n] {};
+    for(int i = 0; i < m; i++) {
         int x,y;
         cin >> x >> y;
         g[x-1].push_back(y-1);
         g[y-1].push_back(x-1);
     }
-        mark = new int[n];
-        fill_n(mark,n,1e9);
-    for(int i = 0; i < n; i++){
-        if(!mark[i])
-            ans = bfs(i);
-        if(ans)
-    }
+
+    for(int i = 0; i < n; i++)
+        if(!r[i]) {
+            pair<int,int> p = F(i,r);
+            if(p != {-1,-1}) {
+                F(p.first,s);
+                F(p.second,t);
+                int mn = 1e9,idx = -1;
+                for(int i = 0; i < n; i++)
+                    if(s[i] && s[i] == f[i] && s[i] < mn) {
+                        mn = s[i];
+                        idx = i;
+                    }
+
+            }
+        }
+
     return 0;
 }
